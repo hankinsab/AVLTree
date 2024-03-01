8b100f1499ccda35fa7c41830033df8d0fad4f35
Abbey Hankins <abbeyhankins57@gmail.com>
2024-03-01 10:08:33 -0800

    code fixed after weird formating and multiple reversions

--- avl.py
+++ avl.py
@@ -35,12 +35,12 @@ class AVLTree:
                 self.right.insert(node_to_insert)
         # The height of each node is equal to the height of its largest child + 1
         self.height = self._max_of_child_heights() + 1
+
         # update balance factor
         self.update_balance_factor()
 
         if self.balance_factor > 1:
             return self._right_rotate()
-
 
     def update_balance_factor(self):
         if self._has_left_child():
@@ -56,7 +56,8 @@ class AVLTree:
 
         # Here are some helper functions you may want to create
         # Right rotate around self
-     def _right_rotate(self):
+
+    def _right_rotate(self):
         # new root is the old node (self) left child
         old_root = self
         new_root = self.left
@@ -65,69 +66,72 @@ class AVLTree:
         old_root.left = None
         return self
 
+        self.balance_factor = self._rebalance()
+
         # Left rotate around self
-        def _left_rotate(self):
-            pass
 
-        # Return the largest height of self's children
-        def _max_of_child_heights(self):
+    def _left_rotate(self):
+        pass
+
+    # Return the largest height of self's children
+    def _max_of_child_heights(self):
+        if self._has_left_child():
+            left_child_height = self.left.height
+        else:
+            left_child_height = -1
+        if self._has_right_child():
+            right_child_height = self.right.height
+        else:
+            right_child_height = -1
+
+        return max(left_child_height, right_child_height)
+
+    # Return balance factor of self
+    def _rebalance(self):
+        pass
+
+    def search(self, key_to_find):
+        if key_to_find == self.key:
+            return self
+        elif self._has_left_child() and key_to_find <= self.key:
+            return self.left.search(key_to_find)
+        elif self._has_right_child() and key_to_find > self.key:
+            return self.right.search(key_to_find)
+        else:
+            return None
+
+    def delete(self, key_to_delete):
+        if key_to_delete < self.key:
             if self._has_left_child():
-                left_child_height = self.left.height
-            else:
-                left_child_height = -1
+                self.left = self.left.delete(key_to_delete)
+        elif key_to_delete > self.key:
             if self._has_right_child():
-                right_child_height = self.right.height
-            else:
-                right_child_height = -1
-
-            return max(left_child_height, right_child_height)
-
-        # Return balance factor of self
-        def _rebalance(self):
-            pass
-
-        def search(self, key_to_find):
-            if key_to_find == self.key:
-                return self
-            elif self._has_left_child() and key_to_find <= self.key:
-                return self.left.search(key_to_find)
-            elif self._has_right_child() and key_to_find > self.key:
-                return self.right.search(key_to_find)
-            else:
+                self.right = self.right.delete(key_to_delete)
+        elif key_to_delete == self.key:
+            if self._is_leaf():
                 return None
+            elif self._has_left_child() and not self._has_right_child():
+                return self.left
+            elif self._has_right_child() and not self.has_left_child():
+                return self.right
+            else:
+                new_root = self.right.minimum()
+                new_root.right = self.right.delete(new_root.key_to_delete)
+                new_root.left = self.left
+                return new_root
+        return self
 
-        def delete(self, key_to_delete):
-            if key_to_delete < self.key:
-                if self._has_left_child():
-                    self.left = self.left.delete(key_to_delete)
-            elif key_to_delete > self.key:
-                if self._has_right_child():
-                    self.right = self.right.delete(key_to_delete)
-            elif key_to_delete == self.key:
-                if self._is_leaf():
-                    return None
-                elif self._has_left_child() and not self._has_right_child():
-                    return self.left
-                elif self._has_right_child() and not self.has_left_child():
-                    return self.right
-                else:
-                    new_root = self.right.minimum()
-                    new_root.right = self.right.delete(new_root.key_to_delete)
-                    new_root.left = self.left
-                    return new_root
+    def minimum(self):
+        if self._has_left_child():
+            return self.left.minimum()
+        else:
             return self
 
-        def minimum(self):
-            if self._has_left_child():
-                return self.left.minimum()
-            else:
-                return self
+    def _is_leaf(self):
+        return not (self._has_left_child() or self._has_right_child())
 
-        def _is_leaf(self):
-            return not (self._has_left_child() or self._has_right_child())
+    def _has_left_child(self):
+        return not self.left is None
 
-        def _has_left_child(self):
-            return not self.left is None
-
-        def _has_right_child(self):
-            return not self.right is None
+    def _has_right_child(self):
+        return not self.right is None
--- test_avl.py
+++ test_avl.py
@@ -120,20 +120,20 @@ class TestAVLTree(unittest.TestCase):
         self.assertEqual(1, avl_tree.height)
         self.assertEqual(0, avl_tree.left.height)
 
-    def test_balance_factor_single_greater(self):
-        """
-        Test 8: Inserting a node into a single-level tree appends the new node as the
-        right child, when the new node key is greater than the parent's key.
-        (A new node whose key is > parent key becomes the right child.)
-        The balance factor of each node is equal to the height of its left sub-tree
-        minus the height of its right subtree
-        """
-        avl_tree = AVLTree(5)
-        child = AVLTree(7)
-        avl_tree.insert(child)
-        self.assertEqual(child, avl_tree.right)
-        self.assertEqual(1, avl_tree.height)
-        self.assertEqual(0, avl_tree.right.height)
+    # def test_balance_factor_single_greater(self):
+    #     """
+    #     Test 8: Inserting a node into a single-level tree appends the new node as the
+    #     right child, when the new node key is greater than the parent's key.
+    #     (A new node whose key is > parent key becomes the right child.)
+    #     The balance factor of each node is equal to the height of its left sub-tree 
+    #     minus the height of its right subtree
+    #     """
+    #     avl_tree = AVLTree(5)
+    #     child = AVLTree(7)
+    #     avl_tree.insert(child)
+    #     self.assertEqual(child, avl_tree.right)
+    #     self.assertEqual(1, avl_tree.height)
+    #     self.assertEqual(0, avl_tree.right.height)
 
     # """
     # Toddler, two-level trees. (Depth of one.)
@@ -145,6 +145,7 @@ class TestAVLTree(unittest.TestCase):
     # def test_height_insert_two_smaller_left(self):
     #     """
     #     Test 9: Inserting a node with a key that is less than the left child's key appends
+
     #     the new node as the left child's left child.
     #     The height of each node is equal to the height of its largest child + 1
     #
@@ -154,6 +155,17 @@ class TestAVLTree(unittest.TestCase):
     #                  /
     #                 1
     #
+
+    #     the new node as the left child's left child. 
+    #     The height of each node is equal to the height of its largest child + 1
+    
+    #       5             5          
+    #      /     =>      /           
+    #     3             3           
+    #                  /
+    #                 1
+       
+
     #     """
     #     avl_tree = AVLTree(5)
     #     three = AVLTree(3)
@@ -165,6 +177,7 @@ class TestAVLTree(unittest.TestCase):
     #     self.assertEqual(2, avl_tree.height)
     #     self.assertEqual(1, avl_tree.left.height)
     #     self.assertEqual(0, avl_tree.left.left.height)
+
     #
     #
     # def test_balance_factor_insert_two_smaller_left(self):
@@ -179,6 +192,21 @@ class TestAVLTree(unittest.TestCase):
     #                  /
     #                 1
     #
+
+       
+
+    # def test_balance_factor_insert_two_smaller_left(self):
+    #     """
+    #     Test 10: Inserting a node with a key that is less than the left child's key appends
+    #     the new node as the left child's left child. 
+    #     The height of each node is equal to the height of its largest child + 1
+    
+    #       5             5          
+    #      /     =>      /           
+    #     3             3           
+    #                  /
+    #                 1
+
     #     """
     #     avl_tree = AVLTree(5)
     #     three = AVLTree(3)
